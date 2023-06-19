39f066cf14670911945309b00075d285219a95e0
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-06-19 19:02:50 -0400

    Add SignIn Page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,8 @@ import { Icons } from "./Icons"
 import { buttonVariants } from "./ui/Button"
 
 const NavBar = () => {
-    return <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+    return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
         <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
             {/* logo */}
             <Link href='/' className="flex gap-2 items-center">
@@ -16,6 +17,6 @@ const NavBar = () => {
             <Link href='/sign-in' className={buttonVariants()}>Sign In</Link>
         </div>
     </div>
-    }
+    )}
 
 export default NavBar
