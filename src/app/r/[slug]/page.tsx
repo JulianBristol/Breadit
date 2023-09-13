cc1c7305a975237522024612c265bae4760cb12c
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-09-13 14:48:01 -0400

    Add MiniCreatePost reusable component

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -37,7 +37,7 @@ const page = async ({ params }: PageProps) => {
 	return (
 		<>
 			<h1 className='font-bold text-3xl md:text-4xl h-14'>r/{subreddit.name}</h1>
-			
+			<MiniCreatePost session={session} />
 		</>
 	);};
 
