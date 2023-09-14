a026bd990c2ff5251b4296ee242fa10aa1b99549
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-09-14 16:29:41 -0400

    Add Join Subreddit button

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,3 +1,4 @@
+import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
@@ -81,6 +82,10 @@ const Layout = async ({
 								<div className="flex justify-between gap-x-4 py-3">
 									<p className="text-gray-500">You created the community</p>
 								</div>
+							): null}
+
+							{subreddit.creatorId !== session?.user.id ? (
+								<SubscribeLeaveToggle />
 							): null}
 						</dl>
 					</div>
