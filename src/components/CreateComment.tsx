3b188d2cf618cc9bd7f7fe44a99e7a4753802302
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-10-05 22:50:45 -0400

    Add Comment posting functionality

--- src/components/CreateComment.tsx
+++ src/components/CreateComment.tsx
@@ -2,21 +2,53 @@
 
 import React, { FC, useState } from "react";
 import { Label } from "./ui/Label";
-import { Textarea } from "./ui/textarea";
+import { Textarea } from "./ui/Textarea";
 import { Button } from "./ui/Button";
 import { useMutation } from "@tanstack/react-query";
+import { CommentRequest } from "@/lib/validators/comment";
+import axios, { AxiosError } from "axios";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 interface CreateCommentProps {
-  
+    postId: string;
+    replyToId?: string;
 }
 
-const CreateComment: FC<CreateCommentProps> = ({}) => {
+const CreateComment: FC<CreateCommentProps> = ({postId, replyToId}) => {
 
-	const [input, setInput] = useState<string>();
+	const [input, setInput] = useState<string>("");
+	const router = useRouter();
+	const { loginToast } = useCustomToast();
 
-	const {} = useMutation({
-		mutationFn: async () => {
-            
+	const {mutate: postComment, isLoading} = useMutation({
+		mutationFn: async ({postId, text, replyToId}: CommentRequest) => {
+			const payload: CommentRequest = {
+				postId,
+				text,
+				replyToId
+			};
+
+			const { data } = await axios.patch("/api/subreddit/post/comment", payload);
+			return data;
+		},
+		onError: (err) => {
+			if (err instanceof AxiosError){
+				if (err.response?.status === 401){
+					return loginToast();
+				}
+			}
+
+			return toast({
+				title: "There was a problem",
+				description: "Something went wrong, please try again",
+				variant: "destructive"
+			});
+		},
+		onSuccess: () => {
+			router.refresh();
+			setInput("");
 		}
 	});
 
@@ -33,7 +65,10 @@ const CreateComment: FC<CreateCommentProps> = ({}) => {
 				/>
 
 				<div className="mt-2 flex justify-end">
-					<Button>Post</Button>
+					<Button
+						isLoading={isLoading}
+						disabled={input.length === 0}
+						onClick={() => postComment({ postId, text: input, replyToId })}>Post</Button>
 				</div>
 			</div>
 		</div>
